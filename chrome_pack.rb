ee7d2c7d7259d0c2ba68c2715f3a91330d5d2c5e
Nicholas Strife <nr@fenelon.ru>
2012-08-08 04:51:54 +0400

    Closure Compiler added

--- chrome_pack.rb
+++ chrome_pack.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/ruby
 
+## Chrome Pack v1.0.1
+## Author: Nicholas R, 2012
+
+require 'fileutils'
+
 class ChromePacker
 
 	def initialize(params)
-		path = params[:path]
-		package_name = params[:name]
-		skip = params[:not_minimize]
+		path = params[:path] || params[:p]
+		package_name = params[:name] || params[:n]
+		skip = params[:not_minimize] || params[:nm]
+		type = params[:type] || params[:t]
 
 		if package_name.nil?
 			puts "Package name must be specified"
@@ -16,10 +22,17 @@ class ChromePacker
 		@current_dir_name = File.dirname(@current_dir)
 		@ext_names = [".js", ".css"]
 		@needed_dirs = ["js", "css"]
+		@exclude = ["libs"]
+		@compiler = "#{File.dirname(__FILE__)}/compiler.jar"
 
 		chrome 	= "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 		package = "#{@current_dir_name}/#{package_name}"
 		pem 	= "#{@current_dir_name}/#{package_name}.pem"
+
+		if check(@compiler) === false
+			puts "Compiler not found"
+			return
+		end
 
 		if check(package, true) === false
 			puts "No such package"
@@ -57,11 +70,18 @@ class ChromePacker
 			end
 			puts "Minimization done! Packaging extension..."
 		end
+		command = nil
+		case type
+			when "crx":
+				puts "Creating chrome extension *.crx file"
+				command = "sudo #{chrome_cmd} --pack-extension=#{package} --pack-extension-key=#{pem} --no-message-box"
+			when "zip":
+				puts "Zipping chrome extension into #{@current_dir_name}/#{package_name}.zip"
+				command = "zip -r #{@current_dir_name}/#{package_name}.zip #{package}"
+			else return puts "Type is unavailable" unless command
+		end
 
-		command = "sudo #{chrome_cmd} --pack-extension=#{package} --pack-extension-key=#{pem} --no-message-box"
-		puts "Packing extension: #{command}"
 		command = %x[#{command}]
-		puts "Extension created: #{package}.crx"
 	end
 
 	def read_dir(dir)
@@ -69,24 +89,40 @@ class ChromePacker
 			file_path = "#{dir}/#{entry}"
 			is_file = @ext_names.include?(File.extname(file_path))
 			if entry.match /^([\.\_]+)/ then next end
-			if is_file || File.directory?(file_path)
+			if is_file
 				minimize(file_path, File.extname(file_path)) if is_file
-			end
-			if File.directory?(file_path)
+			elsif File.directory?(file_path) && @exclude.include?(entry) == false
 				puts "## Processing: #{file_path}"
 				read_dir file_path
+			else
+				puts "Skipping #{entry}"
 			end
 		end
 	end
 
 
 	def minimize(file, ext)
-		puts "## Minimizing file: #{file}"
-		file = File.open(file, "r+")
-		temp = file.read.gsub /([\n\t\r]+)/im, ""
-		file.truncate 0
-		file << temp
-		file.close
+		case(ext)
+			when ".js":
+				puts "## Minimizing javascript using Google CC: #{file}"
+				cmd = "sudo java -jar #{@compiler} --js #{file} --js_output_file #{file}-temp"
+				%x[#{cmd}]
+				temp = File.open("#{file}-temp", "r")
+				newfile = File.open(file, "w+")
+				newfile << temp.read.to_s
+				newfile.close
+				temp.close
+				FileUtils.rm("#{file}-temp")
+			else
+				puts "## Minimizing file: #{file}"
+				f = File.open(file, "rb")
+				temp = f.read.to_s.gsub /([\n\t\r]+)/im, ""
+				f.close
+
+				f = File.open(file, "w+")
+				f << temp
+				f.close
+		end
 	end
 
 	def check(path, dir=false)
@@ -96,12 +132,16 @@ class ChromePacker
 
 end
 
-arguments = { :path => Dir.pwd, :name => nil, :not_minimize => false }
+arguments = {}
+arguments.update({ :path => nil, :name => nil, :not_minimize => nil, :type => nil })
+arguments.update({ :p => Dir.pwd, :n => nil, :nm => false, :t => "crx" })
 
-ARGV.each do |val|
-	arg = val.split "="
-	arg_key = arg[0].gsub "--", ""
-	arguments[:"#{arg_key}"] = arg[1]
+args = ARGV.join(" ").scan(/--?([a-z]+)(\s|=)([\w\d]+)/)
+
+args.each do |argument|
+	if argument.kind_of? Array and argument.length > 0
+		arguments[:"#{argument[0]}"] = argument[2]
+	end
 end
 
 ChromePacker.new(arguments)
